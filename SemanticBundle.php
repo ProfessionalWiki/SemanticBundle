10086aa2c50cc8727127a6fd826382c459e1ad61
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-09-03 03:16:24 +0200

    List Semantic Bundle on Special:version
    Thanks to @mwjames for the tip on SoftwareInfo hook

--- SemanticBundle.php
+++ SemanticBundle.php
@@ -11,3 +11,7 @@ wfLoadExtensions( [
 
 	'PageForms',
 ] );
+
+$GLOBALS['wgHooks']['SoftwareInfo'][] = function( array &$software ) {
+	$software['[https://github.com/SemanticMediaWiki/SemanticBundle Semantic Bundle]'] = '1.0.0';
+};
