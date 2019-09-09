89564e59c977bc8f58998458104ba31ec5c727a3
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-09-09 06:52:23 +0200

    Fix license

--- SemanticBundle.php
+++ SemanticBundle.php
@@ -26,14 +26,14 @@ wfLoadExtensions( [
 $GLOBALS['wgExtensionCredits']['semantic'][] = [
 	'path' => __FILE__,
 	'namemsg' => 'semantic-bundle-name',
-	'name' => 'AAA-sort',
+	'name' => 'AA Semantic Bundle',
 	'version' => '2.0.0',
 	'author' => [
 		'[https://www.entropywins.wtf/mediawiki Jeroen De Dauw]'
 	],
 	'url' => 'https://github.com/SemanticMediaWiki/SemanticBundle',
 	'description' => 'Bundle that installs and loads Semantic MediaWiki and associated extensions.',
-	'license-name'   => 'GPL-2.0+'
+	'license-name'   => 'GPL-2.0-or-later'
 ];
 
 $GLOBALS['wgMessagesDirs']['SemanticBundle'] = __DIR__ . '/i18n';
