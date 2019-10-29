6be670ae634bbeb5287411c6d6d9f79ee606ca19
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-10-29 04:08:28 +0100

    Update SemanticBundle.php

--- SemanticBundle.php
+++ SemanticBundle.php
@@ -27,7 +27,7 @@ $GLOBALS['wgExtensionCredits']['semantic'][] = [
 	'path' => __FILE__,
 	'namemsg' => 'semantic-bundle-name',
 	'name' => 'AA Semantic Bundle',
-	'version' => '3.0.0',
+	'version' => '3.0.1',
 	'author' => [
 		'[https://www.entropywins.wtf/mediawiki Jeroen De Dauw]'
 	],
